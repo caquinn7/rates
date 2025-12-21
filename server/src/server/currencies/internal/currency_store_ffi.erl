7b0d56dc4122197977484ca7cc459193d158afc9
Christopher Quinn <caquinn7@gmail.com>
2025-12-21 09:07:05 -0600

    docs: adjust comment in match_by_symbol

--- server/src/server/currencies/internal/currency_store_ffi.erl
+++ server/src/server/currencies/internal/currency_store_ffi.erl
@@ -5,5 +5,4 @@ match_by_symbol(TableName, Symbol) ->
     %% Gleam custom types are represented as tuples: {Constructor, Field1, Field2, ...}
     %% Crypto: {crypto, Id, Name, Symbol, Rank}
     %% Fiat: {fiat, Id, Name, Symbol, Sign}
-    %% Both have Symbol as the 4th element (index 3 in 0-based)
     ets:match_object(TableName, {'_', {'_', '_', '_', Symbol, '_'}}).
